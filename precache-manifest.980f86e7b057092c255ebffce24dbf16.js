fea6a8078112a8ec04eb9acc719df61bf15f2fc7
DV <deepavarma.in@gmail.com>
2019-03-03 21:27:07 +0100

    Updates

---
+++ precache-manifest.980f86e7b057092c255ebffce24dbf16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b1aa40dcd7583bbdb19",
+    "url": "/directorymanager/static/js/runtime~main.6b1aa40d.js"
+  },
+  {
+    "revision": "eac21300b5038a22085e",
+    "url": "/directorymanager/static/js/main.eac21300.chunk.js"
+  },
+  {
+    "revision": "4231f3a09639dea8dad4",
+    "url": "/directorymanager/static/js/2.4231f3a0.chunk.js"
+  },
+  {
+    "revision": "eac21300b5038a22085e",
+    "url": "/directorymanager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "4231f3a09639dea8dad4",
+    "url": "/directorymanager/static/css/2.fa3254bd.chunk.css"
+  },
+  {
+    "revision": "18d1b0827d244830830d8b085981eba0",
+    "url": "/directorymanager/index.html"
+  }
+];
